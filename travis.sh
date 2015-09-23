39bafb70855a37ae9e2a92dbe632b73d025bf98b
Andreas <ameistad@gmail.com>
2015-09-23 02:27:04 +0200

    Add tests

--- travis.sh
+++ travis.sh
@@ -2,3 +2,4 @@ rm -Rf my_django_project/
 cookiecutter . --no-input
 pip install -r my_django_project/requirements/testing.txt
 cd my_django_project/
+py.test tests
---
+++ {{cookiecutter.repo_name}}/tests/test_admin.py
@@ -0,0 +1,10 @@
+import pytest
+
+@pytest.mark.django_db
+def test_welcome_django(admin_client):
+    """
+    Asserts whether the admin page sends a 200 HTTP status code as response
+    200 means that the HTTP request was successful.
+    """
+    response = admin_client.get("http://localhost:8000/admin/")
+    assert response.status_code == 200
