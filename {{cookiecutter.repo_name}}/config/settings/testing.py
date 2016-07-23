b92b6960e49460bb49d81ea0ee410deb6bb74c8b
Andreas Meistad <ameistad@gmail.com>
2016-07-23 23:41:59 +0200

    Load env from file

--- {{cookiecutter.repo_name}}/config/settings/testing.py
+++ {{cookiecutter.repo_name}}/config/settings/testing.py
@@ -4,6 +4,11 @@
 
 from .base import *  # noqa
 
+# Load environment files from file in development
+env_file = join(dirname(__file__), 'development.env')
+if exists(env_file):
+    environ.Env.read_env(str(env_file))
+
 DEBUG = True
 
 ALLOWED_HOSTS = []
