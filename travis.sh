9fd72ead0a26c51f0957e1fcbd7913c443969b75
Andreas Meistad <ameistad@gmail.com>
2016-07-25 15:34:36 +0200

    Revert travis, and fix test

--- travis.sh
+++ travis.sh
@@ -2,6 +2,4 @@ rm -Rf my_django_project/
 cookiecutter . --no-input
 pip install -r my_django_project/requirements/testing.txt
 cd my_django_project/
-python manage.py migrate
-python manage.py createsuperuser --username testuser --email testuser@example.com
 py.test tests
