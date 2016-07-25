8abfdce6d5f5d38a3f12916b62134c36a8d13acc
Andreas Meistad <ameistad@gmail.com>
2016-07-25 15:18:42 +0200

    Add tests for custom user model

--- travis.sh
+++ travis.sh
@@ -2,4 +2,6 @@ rm -Rf my_django_project/
 cookiecutter . --no-input
 pip install -r my_django_project/requirements/testing.txt
 cd my_django_project/
+python manage.py migrate
+python manage.py createsuperuser --username testuser --email testuser@example.com
 py.test tests
