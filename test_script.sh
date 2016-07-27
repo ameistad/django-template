8d16ab6b4f49e72bd2d92b9380e1fb3f805a98ea
Andreas Meistad <ameistad@gmail.com>
2016-07-27 19:54:00 +0200

    Rename travis and make yml shorter

--- test_script.sh
+++ test_script.sh
@@ -1,5 +1,4 @@
-rm -Rf my_django_project/
-cookiecutter . --no-input
+cookiecutter . --no-input --overwrite-if-exists
 pip install -r my_django_project/requirements/testing.txt
 cd my_django_project/
 py.test tests
