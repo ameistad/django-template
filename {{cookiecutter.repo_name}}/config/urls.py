ebbcba5f831262c4cc9fe3a71b7ca684fe89817d
Andreas Meistad <ameistad@gmail.com>
2016-07-28 19:32:47 +0200

    Edit things

--- {{cookiecutter.repo_name}}/config/urls.py
+++ {{cookiecutter.repo_name}}/config/urls.py
@@ -5,5 +5,5 @@ from django.views.generic import TemplateView
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
     url(r'^user/', include('apps.user.urls', namespace='user')),
-    url(r'^$', TemplateView.as_view(template_name='start.html'), name='start'),
+    url(r'^$', TemplateView.as_view(template_name='start.html'), name='home'),
 ]
