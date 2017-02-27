449b8000219b78e10152f6582417ed54751dda1a
Andreas Meistad <ameistad@gmail.com>
2017-02-27 20:36:03 +0100

    Change location of .env file

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -1,17 +1,11 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 """
-Hook to rename .env.keep to .env
-after generating the project.
+Simple script to rename .env.keep to .env after generating the project.
 """
 
 import os
 
+os.rename('.env.keep', '.env')
 
-repo_dir = './config/settings'
-old_env_file = os.path.join(repo_dir, '.env.keep')
-new_env_file = os.path.join(repo_dir, '.env')
-
-os.rename(old_env_file, new_env_file)
-
-print("Django project was generated in {{ cookiecutter.repo_name }}.")
+print('Django project was generated in {{ cookiecutter.repo_name }}.')
