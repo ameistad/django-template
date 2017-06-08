b843dd27f65415ba381444f8a903172a1a9db177
ameistad <ameistad@gmail.com>
2017-06-08 23:08:05 +0200

    Edit whitenoise config

--- {{cookiecutter.repo_name}}/config/settings/production.py
+++ {{cookiecutter.repo_name}}/config/settings/production.py
@@ -13,14 +13,16 @@ INSTALLED_APPS += ('gunicorn', )
 
 # Simplified static file serving with whitenoise.
 # https://warehouse.python.org/project/whitenoise/
+WHITENOISE_MIDDLEWARE = ('whitenoise.middleware.WhiteNoiseMiddleware', )
+MIDDLEWARE = WHITENOISE_MIDDLEWARE + MIDDLEWARE
 STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 
 # Email
 DEFAULT_FROM_EMAIL = env('DJANGO_DEFAULT_FROM_EMAIL',
                          default='{{ cookiecutter.repo_name }} <noreply@{{ cookiecutter.domain_name }}>')
 
-INSTALLED_APPS += ("anymail", )
-ANYMAIL = {
-    "MAILGUN_API_KEY": env('DJANGO_MAILGUN_API_KEY'),
-}
-EMAIL_BACKEND = "anymail.backends.mailgun.MailgunBackend"
+# INSTALLED_APPS += ("anymail", )
+# ANYMAIL = {
+#     "MAILGUN_API_KEY": env('DJANGO_MAILGUN_API_KEY'),
+# }
+# EMAIL_BACKEND = "anymail.backends.mailgun.MailgunBackend"
