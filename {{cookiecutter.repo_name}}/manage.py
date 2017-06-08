5c42bf17b1f1f12ee6d7385a9e43b7d008b9a5ef
ameistad <ameistad@gmail.com>
2017-06-08 17:39:39 +0200

    Remove python2 support

--- {{cookiecutter.repo_name}}/manage.py
+++ {{cookiecutter.repo_name}}/manage.py
@@ -7,4 +7,10 @@ if __name__ == "__main__":
 
     from django.core.management import execute_from_command_line
 
+
+    # This allows easy placement of apps within the interior
+    # pydanny_cookiecutter directory.
+    current_path = os.path.dirname(os.path.abspath(__file__))
+    sys.path.append(os.path.join(current_path, 'apps'))
+
     execute_from_command_line(sys.argv)
