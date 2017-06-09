ac5ec0e30f51b5483aaacd566a4b8c02e3138072
ameistad <ameistad@gmail.com>
2017-06-09 23:10:53 +0200

    Add custom admin url

--- {{cookiecutter.repo_name}}/config/urls.py
+++ {{cookiecutter.repo_name}}/config/urls.py
@@ -1,3 +1,4 @@
+from django.conf import settings
 from django.conf.urls import include, url
 from django.contrib import admin
 from django.views.generic import TemplateView
@@ -6,5 +7,7 @@ urlpatterns = [
     url(r'^$', TemplateView.as_view(
         template_name='index.html'),
         name='home'),
-    url(r'^admin/', include(admin.site.urls))
+
+    # Django Admin, use {% raw %}{% url 'admin:index' %}{% endraw %}
+    url(settings.ADMIN_URL, include(admin.site.urls))
 ]
