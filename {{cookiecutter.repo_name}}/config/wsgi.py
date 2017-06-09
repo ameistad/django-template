603b4b6ae8e290f3d57e98bda71c2437e1eda1c9
ameistad <ameistad@gmail.com>
2017-06-09 23:11:37 +0200

    Remove whitenoise

--- {{cookiecutter.repo_name}}/config/wsgi.py
+++ {{cookiecutter.repo_name}}/config/wsgi.py
@@ -9,10 +9,5 @@ https://docs.djangoproject.com/en/stable/howto/deployment/wsgi/
 import os
 
 from django.core.wsgi import get_wsgi_application
-from whitenoise.django import DjangoWhiteNoise
-
-
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings.production")
 
 application = get_wsgi_application()
-application = DjangoWhiteNoise(application)
