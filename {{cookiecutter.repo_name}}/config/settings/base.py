2f56835194a8df3c50dd49d4ee567f19b454d77f
ameistad <ameistad@gmail.com>
2017-06-17 18:14:20 +0200

    Add password validators

--- {{cookiecutter.repo_name}}/config/settings/base.py
+++ {{cookiecutter.repo_name}}/config/settings/base.py
@@ -78,6 +78,23 @@ DATABASES = {
         default='postgres://{% if cookiecutter.windows == 'y' %}localhost{% endif %}/{{ cookiecutter.repo_name }}'),
 }
 
+# Password validation
+# https://docs.djangoproject.com/en/stable/ref/settings/#auth-password-validators
+AUTH_PASSWORD_VALIDATORS = [
+    {
+        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+    },
+]
+
 
 # Internationalization
 # https://docs.djangoproject.com/en/stable/topics/i18n/
